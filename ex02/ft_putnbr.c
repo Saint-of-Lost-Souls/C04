0e9602ca1689258e460a9ee9605c1057c03ead51
Shade St Jude <shade.st.jude84@gmail.com>
2021-12-14 10:37:29 +1030

    update

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -1,27 +1,34 @@
-/* #include <unistd.h>
+#include <unistd.h>
 
-i really dont think i give a fuck.
-ppl are liars. ppl cannot be trusted.
-there must be something in the coolaid because everyone is the fucking same        
-ppl are de-evolving and prove themselves to be worth very little of my time and attention
-dad was right
-no one is entertaining
-no one is educating
-so, i guess, everyone can just fuck off.
-i can't be arsed doing anything right now
-if there wasnt a meaningless exam, id go home and fucken lift shit
-where the hell can i find an opportunity to be.. to feel worthwhile.
-i thought writing a book would hep, get published, feel accomplished.
-well brad fucked that right up, eh?
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
-there's persistence, but then there's stupidity
+void	ft_putnbr(int nb)
+{
+	unsigned int	i;
 
-constantly getting back up after failure, is one thing
-constantly 'coming back' after rejection?
-yeah, that's fucken stupid
-and this place is a petri dish of rejection
-for someone like me
+	i = 0;
+	if (nb < 0)
+	{
+		ft_putchar('-'); // if 42 is less than 0, put -, i = -42
+		i = -nb;
+	}
+	else
+	{
+		i = nb;
+	}
+	if (i > 9)
+	{
+		ft_putnbr(i / 10);
+	}
+	ft_putchar(i % 10 + 48);
+}
 
-fuck it's funny still being able to instil feelings of such... cowardice in ppl. true or not, it doesn't matter
+int main()
+{
+    ft_putnbr(10);
+    return 0;
 
- */
+}
